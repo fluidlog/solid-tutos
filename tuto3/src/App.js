74eead64390069d1499b07de689c5d906a80bbde
Yannick DUTHE <yannickduthe@hotmail.com>
2022-07-28 19:00:41 +0200

    Tuto3 - remove item of tuto 2 (users)

--- tuto3/src/App.js
+++ tuto3/src/App.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
-import FetchUriUser from './fetchUriUser'
 import FetchUriGlossary from './fetchUriGlossary'
-import UriFormUser from './uriFormUser'
 import UriFormGlossary from './uriFormGlossary'
 import styled from 'styled-components'
 import {
@@ -22,34 +20,18 @@ function App() {
     margin : 0px;
   `;
 
-  const [uriUser, setUriUser] = useState("https://pod.inrupt.com/fluidlog/public/Carto4CH/users/");
   const [uriGlossary, setUriGlossary] = useState("https://fluidlog.solidcommunity.net/public/glossary/terms.ttl");
-  const [readyToFetchUser, setReadyToFetchUser] = useState(false);
   const [readyToFetchGlossary, setReadyToFetchGlossary] = useState(false);
   const [readyToDisplayEditForm, setReadyToDisplayEditForm] = useState(false);
   const [readyToDisplaySelectedTerm, setReadyToDisplaySelectedTerm] = useState(false);
   const [signification, setSignification] = useState();
   const [uriSelectedTerm, setUriSelectedTerm] = useState();
 
-  const listUriUser =  [
-    { value:"fluidlog",uri:"https://pod.inrupt.com/fluidlog/public/Carto4CH/users/"},
-    { value:"markhoff",uri:"https://pod.inrupt.com/markhoff/public/maCarto4CH/users/"},
-    { value:"marlet",uri:"https://pod.inrupt.com/marlet/public/test4CH/users/"}
-  ]
-
   const listUriGlossary =  [
     { value:"glossary1",uri:"https://fluidlog.solidcommunity.net/public/glossary/terms.ttl"},
     { value:"glossary2",uri:"https://fluidlog.solidcommunity.net/public/glossary/terms2.ttl"},
     { value:"glossary3",uri:"https://fluidlog.solidcommunity.net/public/glossary/terms3.ttl"}
   ]
-
-  const saveUriUser = (uriUser) => {
-		setUriUser(uriUser);
-	};
-
-  const goFetchUser = () => {
-		setReadyToFetchUser(true);
-	};
 
   const saveUriGlossary = (uriGlossary) => {
 		setUriGlossary(uriGlossary);
@@ -114,11 +96,6 @@ function App() {
 							<h2>Documentation</h2>
               Retrouvez la documentation de ce projet dans le PAD : <a className="App-link" href="https://pad.lescommuns.org/Solid-Tutos"> PAD des Solid Tutos </a>
 						</div>
-						<div>
-							<h2>Tuto 1 et 2 - Liste des users</h2>
-              <P1>Vous avez la possibilité d'entrer une URi manuellement</P1>
-							<UriFormUser defaultUriUser={uriUser} saveUriUser={saveUriUser} goFetchUser={goFetchUser} options={listUriUser}/>
-						</div>
             <div>
 							<h2>Tuto 3 - Glossaire</h2>
               <P1>Vous avez la possibilité d'entrer une URi manuellement</P1>
@@ -141,17 +118,7 @@ function App() {
 						</div>
 				</div>
         <div className='flex-large'>
-          { // Affichage de la liste des users
-            readyToFetchUser ? (
-            <div>
-  						<h2>Liste des users</h2>
-              <FetchUriUser uriUser={uriUser} goFetchUser={goFetchUser} />
-            </div>
-          ) : (
-            <h2>Aucune Uri sélectionnée</h2>
-          )}
-
-          { // Affichage de la liste du lexique
+            { // Affichage de la liste du lexique
             readyToFetchGlossary ? (
             <div>
   						<h2>Liste du lexique</h2>
@@ -169,7 +136,7 @@ function App() {
               />
             </div>
           ) : (
-            <h2>Aucune Uri sélectionnée</h2>
+            <h2>Aucun lexique sélectionné</h2>
           )}
         </div>
       </div>
